0c0dd7ae34cbfae7eacb8506a505112e28b7c075
Jarom Diaz <jarom3429@gmail.com>
2021-11-15 11:46:46 -0700

    Added Artifact Class Functionality

--- rfk/game/casting/artifact.py
+++ rfk/game/casting/artifact.py
@@ -1,3 +1,17 @@
 from game.casting.actor import Actor
 
 # TODO: Implement the Artifact class here. Don't forget to inherit from Actor!
+# WE NEED: get_message() & set_message()
+
+class Artifact(Actor):
+    
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+
+    def get_message(self):
+        return self._message
+
+    def set_message(self, message):
+        self._message = message
+
